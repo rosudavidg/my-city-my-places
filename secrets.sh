19da5d5466cad9178483336d2393d173fc8a100f
rosudavidg <rosudavidg@gmail.com>
2021-01-25 19:30:34 +0200

    add google maps key replacement functionality

--- secrets.sh
+++ secrets.sh
@@ -7,9 +7,12 @@ mkdir secrets &> /dev/null
 read -p "Enter database name: " database_db
 read -p "Enter user: " database_user
 read -s -p "Enter password: " database_password
+echo ''
 read -p "Enter email address: " email_address
 read -s -p "Enter email password: " email_password
+echo ''
 read -p "Enter jwt key: " auth_jwt_key
+read -p "Enter Google Maps API key: " google_maps_api_key
 
 # Create secret files
 echo $database_db > secrets/database_db
@@ -17,4 +20,7 @@ echo $database_user > secrets/database_user
 echo $database_password > secrets/database_password
 echo $email_address > secrets/email_address
 echo $email_password > secrets/email_password
-echo $auth_jwt_key > secret/auth_jwt_key
+echo $auth_jwt_key > secrets/auth_jwt_key
+
+# Replace google maps key in docker-compose file
+sed -i '' 's/<REPLACE WITH KEY>/'"$google_maps_api_key"'/g' docker-compose.yml
